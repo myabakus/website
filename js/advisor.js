f49758cf82c79833eac5bd8d03b7866c8065e251
Loduis Madariaga <loduis@gmail.com>
2017-10-14 11:09:06 -0500

    go to plan list when load pricing page.

--- js/advisor.js
+++ js/advisor.js
@@ -1,8 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const element = document.querySelector('#pricing + .content-block');
+  if (element !== null) {
+    element.scrollIntoView(true);
+  }
+}, false);
+
 drift.on('ready',function(api, payload) {
   document.addEventListener('click', (event) => {
-     const target = event.target
-     if (target.tagName == 'A' && target.classList.contains('advisor')) {
-         api.sidebar.open()
-     }
-  })
- })
+    const target = event.target;
+    if (target.tagName == 'A' && target.classList.contains('advisor')) {
+      api.sidebar.open();
+    }
+  });
+});
