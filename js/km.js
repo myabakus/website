07c7e521e8660c1888d4c04bd5577e27a633e932
Loduis Madariaga <loduis@gmail.com>
2019-07-29 08:14:36 -0500

    store campaign info.

--- js/km.js
+++ js/km.js
@@ -1,23 +1,28 @@
-/*
-var _kmq = _kmq || [];
-var _kmk = _kmk || 'cb2d86b4cc3a849919e2393125e5b5f77855e05b';
-function _kms(u){
-  setTimeout(function(){
-    var d = document, f = d.getElementsByTagName('script')[0],
-    s = d.createElement('script');
-    s.type = 'text/javascript'; s.async = true; s.src = u;
-    f.parentNode.insertBefore(s, f);
-  }, 1);
-}
-_kms('//i.kissmetrics.com/i.js');
-_kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
-*/
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','_ga');
-
 _ga('create', 'UA-2211383-1', 'auto');
 _ga('require', 'GTM-W5PT3GW');
 _ga('send', 'pageview');
+
+(() => {
+  if (document.location.search && sessionStorage !== null) {
+    if (getUrlParam('gclid') && sessionStorage.getItem('_ga') === null) {
+      const compaign = strip(getUrlParam('campaign'));
+      const keyword = strip(getUrlParam('keyword'));
+      const params = { compaign, keyword };
+      sessionStorage.setItem('_ga', JSON.stringify(params));
+    }
+  }
+
+  function getUrlParam(param) {
+    var match = document.location.search.match('(?:\\?|&)' + param + '=([^&#]*)');
+    return (match && match.length == 2) ? decodeURIComponent(match[1]) : '';
+  }
+
+  function strip(string) {
+    return string.replace(/\+/g, ' ').trim().replace(/\s+\s/g, ' ');
+  }
+
+})();
--- js/main.js
+++ js/main.js
@@ -455,29 +455,17 @@
     }
     */
 
-    function _uGC(l,n,s) {
-      if (!l || l=="" || !n || n=="" || !s || s=="") return "-";
-      var i,i2,i3,c="-";
-      i=l.indexOf(n);
-      i3=n.indexOf("=")+1;
-      if (i > -1) {
-       i2=l.indexOf(s,i); if (i2 < 0) { i2=l.length; }
-       c=l.substring((i+i3),i2);
-      }
-      return c;
-    }
-
     function _getGAVar(name) {
-      var z = _uGC(document.cookie, '__utmz=', ';');
-      switch(name) {
-        case 'campaing':
-          name = 'utmccn';
-          break;
-        case 'keyword':
-          name = 'utmctr';
-          break;
+      let ga;
+      if (typeof sessionStorage === 'undefined' || (ga = sessionStorage.getItem('_ga')) === null) {
+        return '';
       }
-      return _uGC(z, name + '=', '|');
+      try {
+        ga = JSON.parse(ga);
+        return ga[name] || '';
+      } catch (e) {
+        return '';
+      }
     }
 
     function done(data) {
@@ -522,7 +510,7 @@
     return function (field) {
       _overaly('show');
       var $form = _element(field);
-      $('campaing').val(_getGAVar('campaing'));
+      $('campaing').val(_getGAVar('campaign'));
       $('keyword').val(_getGAVar('keyword'));
       $.post($.route('/app/signup', true), $form.serialize())
            .done(done)
