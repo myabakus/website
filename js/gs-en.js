58c48933f6c9df6741e23bb9d38ff5ca113b3e91
fedepena <federico@myabakus.com>
2020-05-16 10:51:37 +1200

    Update website
    Adjusted headlines, blocks order, key terms, pricing and electronic invoicing plus changed the page format for the getting started guide to grid with player

---
+++ js/gs-en.js
@@ -0,0 +1,86 @@
+var data = {
+  "title": "Getting Started Guide",
+  "subtitle": "The purpose of this guide is to show you step by step where the information in the demo company comes from. Although the demo company gives you a good idea of what an account looks like with data, it does not tell you how the information was entered or what is the best way to do it. That is why we have created this guide: so you can see the process step by step and use it in the evaluation and implementation of the software in your business.",
+  "tags": {
+    "*": "All",
+    "gettingstarted": "Getting started",
+    "invoicing": "Invoicing",
+    "income": "Income",
+    "expenses": "Expenses",
+    "cash": "Cash",
+    "inventory": "Inventory",
+    "reports": "Reports",
+    "receivable": "Receivable",
+    "payable": "Payable",
+    "items": "Items",
+    "import": "Import",
+    "export": "Export"
+  },
+  "url": "../videos/",
+  "videos": [
+    {
+      "title": "Introduction: How to orient yourself in the application",
+      "desc": "A quick review of the different sections in the application and how to move around",
+      "lang": "en",
+      "id": "cYe1cVx6a2",
+      "icon": "1",
+      "tags": "items invoicing import"
+    },
+    {
+      "title": "Step 1: Enter the initial inventory",
+      "desc": "Let's start by created the products we have in inventory with the stock quantities",
+      "lang": "en",
+      "id": "cYeh09x64K",
+      "icon": "2",
+      "tags": "invoicing"
+    },
+    {
+      "title": "Step 2: Enter the starting cash balances",
+      "desc": "The key to reliable financial information is to enter cash balances and keep them in sync with reality",
+      "lang": "en",
+      "id": "cYe6qqxYgw",
+      "icon": "3",
+      "tags": "income reports items"
+    },
+    {
+      "title": "Step 3: Enter purchases of products",
+      "desc": "We are going to increase our stock levels by purchasing from our suppliers",
+      "lang": "en",
+      "id": "cYeq3ByVxz",
+      "icon": "4",
+      "tags": "export"
+    },
+    {
+      "title": "Step 4: Enter operating expenses",
+      "desc": "Let's enter recurring expenses such as rent and payment to employees and contractors",
+      "lang": "en",
+      "id": "cYe0rXyZzj",
+      "icon": "5",
+      "tags": "income"
+    },
+    {
+      "title": "Step 5: Enter sales of products",
+      "desc": "Now we are going to reduce our stock levels selling products to our customers",
+      "lang": "en",
+      "id": "cYfeeE4Jjl",
+      "icon": "6",
+      "tags": "inventory"
+    },
+    {
+      "title": "Step 6: Enter financial transactions",
+      "desc": "This transactions are vital for the business cash flow, despite not being income or expenses",
+      "lang": "en",
+      "id": "cYf2Ynz1VJ",
+      "icon": "1",
+      "tags": "invoicing"
+    },
+    {
+      "title": "Step 7: Enter financial income and expenses",
+      "desc": "The unavoidable sidde effect of financial transactions is that they have a cost and it can be signficant",
+      "lang": "en",
+      "id": "cYhf3sBLGC",
+      "icon": "2",
+      "tags": "gettingstarted inventory"
+    }
+  ]
+};
---
+++ js/gs-es.js
@@ -0,0 +1,85 @@
+var data = {
+  "title": "Guia de Arranque",
+  "subtitle": "El objetivo de este guía es mostrarle paso por paso de dónde viene la información de la empresa demo. Aunque la empresa demo le da una buena idea de como se ve una cuenta con datos, no le muestra cómo se ingresó la información ni cuál es la mejor forma de hacerlo. Por eso hemos creado esta guía: para que usted puede ver el proceso paso por paso y aplicarlo en la evaluación e implementación del software en su empresa.",
+  "tags": {
+    "*": "Todos",
+    "arrancando": "Arrancando",
+    "facturacion": "Facturación",
+    "ingresos": "Ingresos",
+    "gastos": "Gastos",
+    "efectivo": "Efectivo",
+    "inventario": "Inventario",
+    "reportes": "Reportes",
+    "porcobrar": "Por Cobrar",
+    "porpagar": "Por Pagar",
+    "items": "Items",
+    "importar": "Importar",
+    "exportar": "Exportar"
+  },
+  "videos": [
+    {
+      "title": "Introducción: Cómo orientarse en la aplicación",
+      "desc": "Un recorrido rápido de las diferentes secciones de la aplicación y cómo recorrela",
+      "lang": "es",
+      "id": "cYnT3YwMui",
+      "icon": "1",
+      "tags": "impuestos"
+    },
+    {
+      "title": "Paso 1: Ingresar el inventario inicial",
+      "desc": "Comencemos creando los productos que tenemos inventario con sus correspondientes existencias",
+      "lang": "es",
+      "id": "cYeh0wx6xn",
+      "icon": "2",
+      "tags": "impuestos"
+    },
+    {
+      "title": "Paso 2: Ingresar los saldos iniciales de efectivo",
+      "desc": "Ingresar los saldos de efectivo y mantenerlos conciliados es la clave para tener información confiable",
+      "lang": "es",
+      "id": "cYe6qOxYk8",
+      "icon": "4",
+      "tags": "facturacion cuentasxcobrar"
+    },
+    {
+      "title": "Paso 3: Ingresar compras de productos",
+      "desc": "Vamos a aumentar nuestro inventario de productos haciendole compras a nuestros proveedores",
+      "lang": "es",
+      "id": "cYeFr8xN5y",
+      "icon": "5",
+      "tags": "items facturacion importar"
+    },
+    {
+      "title": "Paso 4: Ingresar gastos operacionales",
+      "desc": "Registremos gastos que ocurren regularmente como arriendo y pagos a empleados",
+      "lang": "es",
+      "id": "cYe33dyDDH",
+      "icon": "6",
+      "tags": "ingresos reportes items"
+    },
+    {
+      "title": "Paso 5: Ingresar ventas de productos",
+      "desc": "Ahora vamos a reducir nuestro inventario de productos haciendole ventas a nuestros clientes",
+      "lang": "es",
+      "id": "cYfeeu4Jhj",
+      "icon": "1",
+      "tags": "exportar"
+    },
+    {
+      "title": "Paso 6: Ingresar transacciones financieras",
+      "desc": "Estas transacciones son vitales para el flujo de caja de la empresa a pesar de no ser ni ingresos ni gastos",
+      "lang": "es",
+      "id": "cYf2Yhz1eA",
+      "icon": "2",
+      "tags": "ingresos"
+    },
+    {
+      "title": "Paso 7: Ingresar ingresos y gastos financieros",
+      "desc": "El efecto secundario e invevitable de las transacciones financieras es que tienen un costo significativo",
+      "lang": "es",
+      "id": "cYhf3EBLCI",
+      "icon": "3",
+      "tags": "inventario"
+    }
+  ]
+};
