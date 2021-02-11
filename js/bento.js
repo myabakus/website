ec8f2c64531e597a461991d70777046733c4b99e
Loduis Madariaga <loduis@gmail.com>
2021-02-11 16:48:41 -0500

    change phone email name.

--- js/bento.js
+++ js/bento.js
@@ -1,4 +1,4 @@
-if (typeof bento$ !== 'undefined') {
+if (location.host === 'www.myabakus.com' && (typeof bento$ !== 'undefined')) {
   const b = bento$; // no se debe remover esto no se que pasa con la minificacion.
   b(function() {
     bento.view();
