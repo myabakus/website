7ed0004d44af6dc7b626208c010316e1e774dae6
Loduis Madariaga <loduis@gmail.com>
2021-03-30 14:23:47 -0500

    fixed campaign.

--- js/gtag.js
+++ js/gtag.js
@@ -1,3 +1,4 @@
+// https://www.myabakus.com/colombia?utm_source=Facebook&utm_medium=fb&utm_campaign=23846023121650183&utm_content=23847154742320183&utm_term=Nuevo%20anuncio&fbclid=IwAR2XTgWSVrIn1rQznHWcKrLbl2R6GAWblVllKZRaCy4knmbQdoDOWzZGKXo
 if (location.host === 'www.myabakus.com') {
   (function(w,d,s,l,i){
     w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -15,17 +16,19 @@ if (location.host === 'www.myabakus.com') {
 
   (() => {
     if (document.location.search && sessionStorage !== null) {
-      if (getUrlParam('gclid') && sessionStorage.getItem('_ga') === null) {
-        const compaign = strip(getUrlParam('campaign'));
-        const keyword = strip(getUrlParam('keyword'));
-        const params = { compaign, keyword };
+      const isAds = getUrlParam('gclid') || getUrlParam('fbclid');;
+      if (isAds && sessionStorage.getItem('_ga') === null) {
+        const campaign = getUrlParam('utm_campaign');
+        const keyword = getUrlParam('utm_term') || getUrlParam('utm_keyword');
+        const source = getUrlParam('utm_source').toLowerCase();
+        const params = { campaign, keyword: source + ': ' + keyword };
         sessionStorage.setItem('_ga', JSON.stringify(params));
       }
     }
 
     function getUrlParam(param) {
-      var match = document.location.search.match('(?:\\?|&)' + param + '=([^&#]*)');
-      return (match && match.length == 2) ? decodeURIComponent(match[1]) : '';
+      var match = document.location.search.match(param + '=([^&#]*)');
+      return (match && match.length == 2) ? strip(decodeURIComponent(match[1])) : '';
     }
 
     function strip(string) {
