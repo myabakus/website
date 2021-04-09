257da350535cbd5dc3af8e0fb065d61e35f30431
Loduis Madariaga <loduis@gmail.com>
2021-04-09 11:53:40 -0500

    remove tailwind keyframe

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,35 +1,35 @@
-const { fontSize, fontFamily } = require('tailwindcss/defaultTheme')
+const { fontSize, fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    '../articles/*.html',
-    '../articulos/*.html'
-  ],
+  purge: {
+    content: ["../articles/*.html", "../articulos/*.html"],
+    options: {
+      keyframes: true,
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Quattrocento Sans', 'sans-serif'],
-        heading: ['Paytone One', 'sans-serif'],
+        sans: ["Quattrocento Sans", "sans-serif"],
+        heading: ["Paytone One", "sans-serif"],
       },
       typography: {
         DEFAULT: {
           css: {
-            fontSize: '1.125rem',
+            fontSize: "1.125rem",
             h1: {
               // fontSize: '2.25rem',
-              fontFamily: 'Paytone One',
+              fontFamily: "Paytone One",
             },
             // ...
           },
         },
       },
-    }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
